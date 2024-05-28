64798c6cf9c0ca426aaeffb7b9a0a1ce0b02a65e
Liam Teale <liamteale6@gmail.com>
2024-05-28 09:08:44 -0700

    add documentation to Pose class

--- src/units/Pose.cpp
+++ src/units/Pose.cpp
@@ -10,4 +10,6 @@ Pose::Pose(V2Position v, Angle h) : V2Position(v), theta(0.0) {}
 Pose::Pose(Length nx, Length ny, Angle nh) : V2Position(nx, ny), theta(nh) {}
 
 Angle Pose::getTheta() { return theta; }
+
+void Pose::setTheta(Angle h) { theta = h; }
 } // namespace units
