6e5d91292ccd0033153c68bc5cb4c3fbbd18a23e
lufimio <67525443+PA055@users.noreply.github.com>
2024-05-31 20:21:25 -0400

    fixed bug in _temp operators

--- include/units/Temperature.hpp
+++ include/units/Temperature.hpp
@@ -6,21 +6,25 @@ using Temperature = Quantity<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::r
                              std::ratio<0>, std::ratio<0>>;
 
 constexpr Temperature kelvin = Temperature(1.0);
-constexpr Temperature celcius = Temperature(1.0);
-constexpr Temperature farenheight = Temperature(1.8);
+constexpr Temperature celcius = Temperature();
+constexpr Temperature farenheight = Temperature();
 
 constexpr Temperature operator""_kelvin(long double value) { return static_cast<double>(value) * kelvin; }
 
 constexpr Temperature operator""_kelvin(unsigned long long value) { return static_cast<double>(value) * kelvin; }
 
-constexpr Temperature operator""_celcius(long double value) { return static_cast<double>(value) * kelvin; }
+constexpr Temperature operator""_celcius(long double value) { return (static_cast<double>(value) + 273.15) * kelvin; }
 
-constexpr Temperature operator""_celcius(unsigned long long value) { return static_cast<double>(value) * kelvin; }
+constexpr Temperature operator""_celcius(unsigned long long value) {
+    return (static_cast<double>(value) + 273.15) * kelvin;
+}
 
-constexpr Temperature operator""_farenheight(long double value) { return static_cast<double>(value) * farenheight; }
+constexpr Temperature operator""_farenheight(long double value) {
+    return ((static_cast<double>(value) - 32) * (5.0 / 9.0) + 273.5) * kelvin;
+}
 
 constexpr Temperature operator""_farenheight(unsigned long long value) {
-    return static_cast<double>(value) * farenheight;
+    return ((static_cast<double>(value) - 32) * (5.0 / 9.0) + 273.5) * kelvin;
 }
 
 namespace units {
@@ -33,10 +37,8 @@ constexpr inline Temperature from_celcius(double value) { return Temperature(val
 
 constexpr inline double to_celcius(Temperature quantity) { return quantity.val() - 273.15; }
 
-constexpr inline Temperature from_farenheight(double value) { return (value - 32) / farenheight.val() + 273.15; }
+constexpr inline Temperature from_farenheight(double value) { return (value - 32) * (5.0 / 9.0) + 273.15; }
 
-constexpr inline double to_farenheight(Temperature quantity) {
-    return (quantity.val() - 273.15) * farenheight.val() + 32;
-}
+constexpr inline double to_farenheight(Temperature quantity) { return (quantity.val() - 273.15) * (9.0 / 5.0) + 32; }
 
 } // namespace units
