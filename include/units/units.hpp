0d33c009e4539779f7098d642af229890ce1f7bc
ion098 <146852218+ion098@users.noreply.github.com>
2024-12-23 10:34:52 -0800

    fix: :bug: Add missing operator-() for Quantity

--- include/units/units.hpp
+++ include/units/units.hpp
@@ -211,6 +211,11 @@ template <isQuantity Q, isQuantity R> constexpr Q operator-(Q lhs, R rhs)
     return Q(lhs.internal() - rhs.internal());
 }
 
+template <isQuantity Q> constexpr Q operator-(Q quantity)
+{
+    return Q(-quantity.internal());
+}
+
 template <isQuantity Q> constexpr Q operator*(Q quantity, double multiple) { return Q(quantity.internal() * multiple); }
 
 template <isQuantity Q> constexpr Q operator*(double multiple, Q quantity) { return Q(quantity.internal() * multiple); }
