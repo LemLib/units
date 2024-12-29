ceb95b5aede1353f6b43d36bd4ee8d3adf51905a
Liam Teale <liamteale6@gmail.com>
2024-12-28 17:34:37 -0800

    remove unnecessary `from_##suffix` literal

--- include/units/units.hpp
+++ include/units/units.hpp
@@ -299,7 +299,6 @@ template <isQuantity Q, isQuantity R> constexpr bool operator>(const Q& lhs, con
     [[maybe_unused]] constexpr Name suffix = multiple;                                                                 \
     constexpr Name operator""_##suffix(long double value) { return static_cast<double>(value) * multiple; }            \
     constexpr Name operator""_##suffix(unsigned long long value) { return static_cast<double>(value) * multiple; }     \
-    constexpr inline Name from_##suffix(double value) { return value * multiple; }                                     \
     constexpr inline Name from_##suffix(Number value) { return value.internal() * multiple; }                          \
     constexpr inline double to_##suffix(Name quantity) { return quantity.convert(multiple); }
 
